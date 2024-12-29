e8c14f14c812365734b82efb5791c6ace577615b
AngelIpina12 <angelipina12@hotmail.com>
2024-12-29 17:54:50 -0600

    Add firs part of method login in user controller

--- controllers/user.js
+++ controllers/user.js
@@ -40,12 +40,49 @@ const register = async (req, res) => {
         return res.status(500).json({
             status: "error",
             message: "An error occurred: " + error,
-            userToSave
+        })
+    }
+}
+
+const login = async (req, res) => {
+    const params = req.body;
+
+    if(!params.email || !params.password){
+        return res.status(400).json({
+            status: "error",
+            message: "One or more data is missing."
+        })
+    }
+    try{
+        const userLogin = await User.findOne({email: params.email});
+        const comparedPassword = bcrypt.compareSync(params.password, userLogin.password)
+        if(!comparedPassword){
+            return res.status(400).json({
+                status: "error",
+                message: "You haven't entered the correct password."
+            })
+        }
+        const token = false
+        return res.status(200).json({
+            status: "success",
+            message: "You have logged in successfully.",
+            user: {
+                id: userLogin._id,
+                name: userLogin.name,
+                nick: userLogin.nick
+            },
+            token
+        })
+    }catch(error){
+        return res.status(500).json({
+            status: "error",
+            message: "An error occurred: " + error,
         })
     }
 }
 
 module.exports = {
     testUser,
-    register
+    register,
+    login
 }
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const UserController = require("../controllers/user")
 
 router.get("/test-user", UserController.testUser);
 router.post("/register", UserController.register);
+router.post("/login", UserController.login);
 
 module.exports = router;
