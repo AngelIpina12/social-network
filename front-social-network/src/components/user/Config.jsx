32711f5445b831d9d1cffd47cef9b8d4471ee922
AngelIpina12 <angelipina12@hotmail.com>
2025-01-06 15:47:59 -0600

    Add submit function for form in Config component

--- front-social-network/src/components/user/Config.jsx
+++ front-social-network/src/components/user/Config.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
 import useAuth from '../../hooks/useAuth';
 import { Global } from '../../helpers/Global';
+import { SerializeForm } from '../../helpers/SerializeForm';
 
 export const Config = () => {
     const { auth } = useAuth();
     const [saved, setSaved] = useState("not_saved");
     const updateUser = (e) => {
         e.preventDefault();
-        console.log(auth);
+        let newDataUser = SerializeForm(e.target);
+        delete newDataUser.file0;
     }
 
     return (
