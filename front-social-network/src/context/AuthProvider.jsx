483cfd7c65ac0d7d8b060e0204eae0fa3bf11846
AngelIpina12 <angelipina12@hotmail.com>
2025-01-06 12:48:13 -0600

    Add context to login in the complete project structure

---
+++ front-social-network/src/context/AuthProvider.jsx
@@ -0,0 +1,36 @@
+import React, { createContext, useEffect, useState } from 'react'
+import { Global } from '../helpers/Global';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({children}) => {
+    const [auth, setAuth] = useState({});
+    useEffect(() => {
+        authUser()
+    }, [])
+    const authUser = async () => {
+        const token = localStorage.getItem("token");
+        const user = localStorage.getItem("user");
+        if (!token || !user) {
+            return false;
+        }
+        const userObj = JSON.parse(user);
+        const userId = userObj.id;
+        const response = await fetch(Global.url + "user/profile/" + userId, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token
+            }
+        });
+        const data = await response.json();
+        setAuth(data.user);
+    }
+  return (
+    <AuthContext.Provider value={{auth,setAuth}}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContext;
--- front-social-network/src/router/Routing.jsx
+++ front-social-network/src/router/Routing.jsx
@@ -5,27 +5,30 @@ import { Login } from '../components/user/Login'
 import { Register } from '../components/user/Register'
 import { PrivateLayout } from '../components/layout/private/PrivateLayout'
 import { Feed } from '../components/publication/Feed'
+import { AuthProvider } from '../context/AuthProvider'
 
 export const Routing = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<PublicLayout />} >
-                    <Route index element={<Login />} />
-                    <Route path='login' element={<Login />} />
-                    <Route path='register' element={<Register />} />
-                </Route>
-                <Route path='/social' element={<PrivateLayout/>}>
-                    <Route index element={<Feed/>}/>
-                    <Route path='feed' element={<Feed/>}/>
-                </Route>
-                <Route path="*" element={
-                    <p>
-                        <h1>Error 404: Page not found</h1>
-                        <Link to='/'>Back to home</Link>
-                    </p>
-                }/>
-            </Routes>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/" element={<PublicLayout />} >
+                        <Route index element={<Login />} />
+                        <Route path='login' element={<Login />} />
+                        <Route path='register' element={<Register />} />
+                    </Route>
+                    <Route path='/social' element={<PrivateLayout />}>
+                        <Route index element={<Feed />} />
+                        <Route path='feed' element={<Feed />} />
+                    </Route>
+                    <Route path="*" element={
+                        <p>
+                            <h1>Error 404: Page not found</h1>
+                            <Link to='/'>Back to home</Link>
+                        </p>
+                    } />
+                </Routes>
+            </AuthProvider>
         </BrowserRouter>
     )
 }
