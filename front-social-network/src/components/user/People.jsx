6166cbeb32db146953b9ef8e93ed00c08c8b99cf
AngelIpina12 <angelipina12@hotmail.com>
2025-01-07 16:04:09 -0600

    Add fetch to list method in API in People component

--- front-social-network/src/components/user/People.jsx
+++ front-social-network/src/components/user/People.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import avatar from '../../assets/img/user.png'
+import { Global } from '../../helpers/Global'
 
 export const People = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    const getUsers = async () => {
+        const response = await fetch(Global.url + 'user/list/1', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')
+            }
+        })
+        const data = await response.json();
+        if (data.users && data.status === "success") {
+            setUsers(data.users);
+        }
+    }
+
+
     return (
         <>
 
@@ -10,46 +31,51 @@ export const People = () => {
             </header>
 
             <div className="content__posts">
+                {users.map(user => {
+                    return (
+                        <article className="posts__post" key={user._id}>
 
-                <article className="posts__post">
+                            <div className="post__container">
 
-                    <div className="post__container">
+                                <div className="post__image-user">
+                                    <a href="#" className="post__image-link">
+                                        {user.image != "default.jpg" && <img src={Global.url + "user/avatar/" + user.image} className="post__user-image" alt="Foto de perfil" />}
+                                        {user.image == "default.jpg" && <img src={avatar} className="post__user-image" alt="Foto de perfil" />}
+                                    </a>
+                                </div>
 
-                        <div className="post__image-user">
-                            <a href="#" className="post__image-link">
-                                <img src={avatar} className="post__user-image" alt="Foto de perfil" />
-                            </a>
-                        </div>
+                                <div className="post__body">
 
-                        <div className="post__body">
+                                    <div className="post__user-info">
+                                        <a href="#" className="user-info__name">{user.name} {user.surname}</a>
+                                        <span className="user-info__divider"> | </span>
+                                        <a href="#" className="user-info__create-date">{user.created_at}</a>
+                                    </div>
 
-                            <div className="post__user-info">
-                                <a href="#" className="user-info__name">Victor Robles</a>
-                                <span className="user-info__divider"> | </span>
-                                <a href="#" className="user-info__create-date">Hace 1 hora</a>
+                                    <h4 className="post__content">{user.bio}</h4>
+
+                                </div>
+
                             </div>
 
-                            <h4 className="post__content">Hola, buenos dias.</h4>
 
-                        </div>
+                            <div className="post__buttons">
 
-                    </div>
-
-
-                    <div className="post__buttons">
-
-                        <a href="#" className="post__button post__button--green">
-                            Follow
-                        </a>
-                        {/* <a href="#" className="post__button">
+                                <a href="#" className="post__button post__button--green">
+                                    Follow
+                                </a>
+                                {/* <a href="#" className="post__button">
                             Unfollow
                         </a> */}
 
-                    </div>
+                            </div>
 
-                </article>
+                        </article>
+                    );
+                })}
 
-                
+
+
             </div>
 
             <div className="content__container-btn">
@@ -57,7 +83,7 @@ export const People = () => {
                     Show more people
                 </button>
             </div>
-
+            <br/>
         </>
     )
 }
