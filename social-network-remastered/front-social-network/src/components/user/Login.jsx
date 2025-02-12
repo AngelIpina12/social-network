a8b36f781ace7e330c1489e3d48b27eb30f44439
AngelIpina12 <angelipina12@hotmail.com>
2025-02-12 17:02:38 -0600

    Add loginUser endpoint in Login component

--- social-network-remastered/front-social-network/src/components/user/Login.jsx
+++ social-network-remastered/front-social-network/src/components/user/Login.jsx
@@ -1,33 +1,33 @@
 import React, { useState } from 'react'
 import { useForm } from '../../hooks/useForm'
-import { Global } from '../../helpers/Global';
 import useAuth from '../../hooks/useAuth';
+import { useLoginUserMutation } from '../../store';
 
 export const Login = () => {
     const { form, changed } = useForm({});
-    const [ logged, setLogged ] = useState("not_logged");
-    const {setAuth} = useAuth();
-    const loginUser = async (e) => {
+    const [logged, setLogged] = useState("not_logged");
+    const { setAuth } = useAuth();
+    const [loginUser] = useLoginUserMutation();
+
+    // Iniciar sesión
+    const handleLogin = async (e) => {
         e.preventDefault();
-        let userToLogin = form;
-        const response = await fetch(Global.url + 'user/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(userToLogin),
-        });
-        const data = await response.json();
-        if(data.status == "success"){
-            localStorage.setItem("token", data.token);
-            localStorage.setItem("user", JSON.stringify(data.user));
-            setLogged("logged");
-            setAuth(data.user);
-            window.location.reload();
-        }else{
+        try {
+            const response = await loginUser({ userLogged: form }).unwrap();
+            if (response.status === "success") {
+                localStorage.setItem("token", response.token);
+                localStorage.setItem("user", JSON.stringify(response.user));
+                setLogged("logged");
+                setAuth(response.user);
+                window.location.reload();
+            } else {
+                setLogged("error");
+            }
+        } catch (error) {
+            console.error("Error al iniciar sesión:", error);
             setLogged("error");
         }
-    }
+    };
 
     return (
         <>
@@ -35,16 +35,24 @@ export const Login = () => {
                 <h1 className="content__title">Login</h1>
             </header>
             <div className='content__posts'>
-                {logged == "logged" ? <strong className='alert alert-success'>User logged successfully!</strong> : ""}
-                {logged == "error" ? <strong className='alert alert-danger'>An error has ocurred. Try again.</strong> : ""}
-                <form className='form-login' onSubmit={loginUser}>
+                {logged === "logged" && (
+                    <strong className='alert alert-success'>
+                        User logged successfully!
+                    </strong>
+                )}
+                {logged === "error" && (
+                    <strong className='alert alert-danger'>
+                        An error has occurred. Try again.
+                    </strong>
+                )}
+                <form className='form-login' onSubmit={handleLogin}>
                     <div className="form-group">
                         <label htmlFor='email'>Email</label>
-                        <input type="email" name="email" onChange={changed}/>
+                        <input type="email" name="email" onChange={changed} />
                     </div>
                     <div className="form-group">
                         <label htmlFor='password'>Password</label>
-                        <input type="password" name="password" onChange={changed}/>
+                        <input type="password" name="password" onChange={changed} />
                     </div>
                     <input type="submit" name='identify' value='Send' className='btn btn-success' />
                 </form>
