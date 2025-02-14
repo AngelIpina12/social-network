4d1f5b0c972d8338a2a7f421c77567d56937bd44
AngelIpina12 <angelipina12@hotmail.com>
2025-02-14 12:12:02 -0600

    Add authSlice reducers in Config component

--- social-network-remastered/front-social-network/src/components/user/Config.jsx
+++ social-network-remastered/front-social-network/src/components/user/Config.jsx
@@ -1,12 +1,12 @@
 import React, { useRef, useState } from 'react'
-import useAuth from '../../hooks/useAuth';
+import { useSelector } from 'react-redux';
 import avatar from '../../assets/img/user.png'
 import { Global } from '../../helpers/Global';
 import { SerializeForm } from '../../helpers/SerializeForm';
 import { useUpdateUserMutation, useUploadUserImageMutation } from '../../store/apis/userApi';
 
 export const Config = () => {
-    const { auth, setAuth } = useAuth();
+    const auth = useSelector((state) => state.auth.user);
     const [updateStatus, setUpdateStatus] = useState("idle");
     const [uploadStatus, setUploadStatus] = useState("idle");
     const fileInputRef = useRef(null);
@@ -23,8 +23,6 @@ export const Config = () => {
         try {
             const response = await updateUserMutation({ userUpdated: newDataUser }).unwrap();
             if (response.status === "success") {
-                const { password, ...userWithoutPassword } = response.user;
-                setAuth(userWithoutPassword);
                 setUpdateStatus("success");
             } else {
                 setUpdateStatus("error");
@@ -38,8 +36,6 @@ export const Config = () => {
             try {
                 const uploadResponse = await uploadUserImageMutation(formData).unwrap();
                 if (uploadResponse.status === "success") {
-                    delete uploadResponse.user.password;
-                    setAuth(uploadResponse.user);
                     setUploadStatus("success");
                 } else {
                     setUploadStatus("error");
@@ -56,47 +52,89 @@ export const Config = () => {
                 <h1 className="content__title">Configuration</h1>
             </header>
             <div className='content__posts'>
-                {updateStatus === "success" && <strong className="alert alert-success">User updated successfully!</strong>}
-                {updateStatus === "error" && <strong className="alert alert-danger">An error has occurred updating user data. Try again.</strong>}
+                {updateStatus === "success" && (
+                    <strong className="alert alert-success">User updated successfully!</strong>
+                )}
+                {updateStatus === "error" && (
+                    <strong className="alert alert-danger">
+                        An error has occurred updating user data. Try again.
+                    </strong>
+                )}
                 {uploadStatus === "loading" && <p>Uploading image...</p>}
                 {uploadStatus === "success" && <p>Image uploaded successfully!</p>}
                 {uploadStatus === "error" && <p>There was an error uploading the image.</p>}
-                <form className='config-form' onSubmit={handleSubmit}>
+                <form className="config-form" onSubmit={handleSubmit}>
                     <div className="form-group">
-                        <label htmlFor='name'>Name</label>
-                        <input type="text" id='name' name="name" placeholder="Enter your name" defaultValue={auth.name} />
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            placeholder="Enter your name"
+                            defaultValue={auth?.name || ''}
+                        />
                     </div>
                     <div className="form-group">
-                        <label htmlFor='surname'>Surname</label>
-                        <input type="text" id='surname' name="surname" placeholder="Enter your surname" defaultValue={auth.surname} />
+                        <label htmlFor="surname">Surname</label>
+                        <input
+                            type="text"
+                            id="surname"
+                            name="surname"
+                            placeholder="Enter your surname"
+                            defaultValue={auth?.surname || ''}
+                        />
                     </div>
                     <div className="form-group">
-                        <label htmlFor='nick'>Nick</label>
-                        <input type="text" id='nick' name="nick" placeholder="Enter your nickname" defaultValue={auth.nick} />
+                        <label htmlFor="nick">Nick</label>
+                        <input
+                            type="text"
+                            id="nick"
+                            name="nick"
+                            placeholder="Enter your nickname"
+                            defaultValue={auth?.nick || ''}
+                        />
                     </div>
                     <div className="form-group">
-                        <label htmlFor='bio'>Bio</label>
-                        <textarea id='bio' name="bio" placeholder="Enter your bio" defaultValue={auth.bio} />
+                        <label htmlFor="bio">Bio</label>
+                        <textarea
+                            id="bio"
+                            name="bio"
+                            placeholder="Enter your bio"
+                            defaultValue={auth?.bio || ''}
+                        />
                     </div>
                     <div className="form-group">
-                        <label htmlFor='email'>Email</label>
-                        <input type="email" id='email' name="email" placeholder="Enter your email" defaultValue={auth.email} />
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Enter your email"
+                            defaultValue={auth?.email || ''}
+                        />
                     </div>
                     <div className="form-group">
-                        <label htmlFor='password'>Password</label>
-                        <input type="password" id='password' name="password" placeholder="Enter your password" />
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name="password" placeholder="Enter your password" />
                     </div>
                     <div className="form-group">
-                        <label htmlFor='file0'>Avatar</label>
+                        <label htmlFor="file0">Avatar</label>
                         <div className="general-info__container-avatar">
-                            {auth.image != "default.jpg" && <img src={Global.url + "user/avatar/" + auth.image} className="container-avatar__img" alt="Foto de perfil" />}
-                            {auth.image == "default.jpg" && <img src={avatar} className="container-avatar__img" alt="Foto de perfil" />}
+                            {auth?.image && auth.image !== "default.jpg" ? (
+                                <img
+                                    src={Global.url + "user/avatar/" + auth.image}
+                                    className="container-avatar__img"
+                                    alt="Foto de perfil"
+                                />
+                            ) : (
+                                <img src={avatar} className="container-avatar__img" alt="Foto de perfil" />
+                            )}
                         </div>
                         <br />
-                        <input type="file" id='file0' name="file0" />
+                        <input type="file" id="file0" name="file0" ref={fileInputRef} />
                     </div>
                     <br />
-                    <input type="submit" value="Update" className='btn btn-success' />
+                    <input type="submit" value="Update" className="btn btn-success" />
                 </form>
                 <br />
             </div>
