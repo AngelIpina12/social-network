bad3da911348f783ba07167e48087e1e6fe45fee
AngelIpina12 <angelipina12@hotmail.com>
2025-03-11 13:04:51 -0600

    Change refactorization of index.jsx store component

--- social-network-remastered/front-social-network/src/store/index.jsx
+++ social-network-remastered/front-social-network/src/store/index.jsx
@@ -1,87 +1,62 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from '@reduxjs/toolkit/query';
-import { followApi } from './apis/followApi';
-import { followReducer } from "./slices/followSlice";
-import { publicationApi } from "./apis/publicationApi";
-import { publicationReducer, getFeedPublications, getUserPublications } from "./slices/publicationSlice";
-import { userApi } from "./apis/userApi";
-import { userReducer } from "./slices/userSlice";
-import { authReducer } from "./slices/authSlice"
-import { counterReducer } from "./slices/counterSlice"
+import { baseApi } from './apis/baseApi';
+import { followReducer, resetFollowState } from "./slices/followSlice";
+import { publicationReducer, resetPublicationState, getFeedPublications, getUserPublications } from "./slices/publicationSlice";
+import { userReducer, resetUsers } from "./slices/userSlice";
+import { authReducer, logout, setUser } from "./slices/authSlice";
+import { counterReducer, updateCounters } from "./slices/counterSlice";
 
+export * from './apis/userApi';
+export * from './apis/publicationApi';
+export * from './apis/followApi';
+
+export {
+  addFollowing,
+  removeFollowing,
+  setFollowings,
+  setFollowers,
+  incrementFollowingsPage,
+  incrementFollowersPage,
+  resetFollowState,
+} from './slices/followSlice';
+
+export {
+  setCurrentPage,
+  setSelectedPublication,
+  clearSelectedPublication,
+  resetPublicationState,
+  getFeedPublications,
+  getUserPublications,
+} from './slices/publicationSlice';
+
+export {
+  resetUsers,
+  setCurrentPage as setUserPage,
+} from './slices/userSlice';
+
+export {
+  logout,
+  setUser,
+} from './slices/authSlice';
+
+export {
+  updateCounters,
+} from './slices/counterSlice';
 
 const store = configureStore({
-    reducer: {
-        followData: followReducer,
-        [followApi.reducerPath]: followApi.reducer,
-        publicationData: publicationReducer,
-        [publicationApi.reducerPath]: publicationApi.reducer,
-        userData: userReducer,
-        [userApi.reducerPath]: userApi.reducer,
-        authData: authReducer,
-        countersData: counterReducer
-    },
-    middleware: (getDefaultMiddleware) => {
-        return getDefaultMiddleware()
-            .concat(followApi.middleware)
-            .concat(publicationApi.middleware)
-            .concat(userApi.middleware)
-    }
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    followData: followReducer,
+    publicationData: publicationReducer,
+    userData: userReducer,
+    authData: authReducer,
+    countersData: counterReducer,
+  },
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 setupListeners(store.dispatch);
-
-export {
-    addFollowing,
-    removeFollowing,
-    setFollowings,
-    setFollowers,
-    incrementFollowingsPage,
-    incrementFollowersPage
-} from './slices/followSlice';
-export {
-    setCurrentPage,
-    setSelectedPublication,
-    clearSelectedPublication,
-    getFeedPublications,
-    getUserPublications
-} from './slices/publicationSlice';
-export {
-    resetUsers,
-    setCurrentPage as setUserPage
-} from './slices/userSlice';
-export {
-    logout,
-    setUser
-} from './slices/authSlice';
-export {
-    updateCounters
-} from './slices/counterSlice'
-export {
-    useFetchUserFollowingsQuery,
-    useFetchUserFollowersQuery,
-    useCreateUserFollowMutation,
-    useDeleteUserFollowMutation
-} from './apis/followApi';
-export {
-    useCreatePublicationMutation,
-    useFetchPublicationDetailQuery,
-    useDeletePublicationMutation,
-    useFetchUserPublicationsQuery,
-    useUploadPublicationImageMutation,
-    useFetchUserFeedQuery,
-    useLazyFetchUserFeedQuery
-} from './apis/publicationApi'
-export {
-    useCreateUserMutation,
-    useLoginUserMutation,
-    useFetchUserProfileQuery,
-    useLazyFetchUserProfileQuery,
-    useFetchListOfUsersQuery,
-    useUpdateUserMutation,
-    useUploadUserImageMutation,
-    useFetchCountersQuery,
-    useLazyFetchCountersQuery
-} from './apis/userApi'
 
 export default store;
