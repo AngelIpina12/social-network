7a95d1219715bc818e6118b0dd6e4d480ce96049
AngelIpina12 <angelipina12@hotmail.com>
2025-03-11 16:09:05 -0600

    Change refactorization of People component

--- social-network-remastered/front-social-network/src/components/user/People.jsx
+++ social-network-remastered/front-social-network/src/components/user/People.jsx
@@ -1,33 +1,73 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { UserList } from './UserList'
-import { useFetchListOfUsersQuery } from '../../store';
-import { resetUsers, setCurrentPage } from '../../store/slices/userSlice';
+import { useFetchListOfUsersQuery, resetUsers, setCurrentPage } from '../../store';
 
 export const People = () => {
     const dispatch = useDispatch();
-    const { currentPage, users, following, pages, loading, error } = useSelector((state) => state.userData);
-    const { data, isLoading, error: fetchError } = useFetchListOfUsersQuery({ page: currentPage });
+    const { currentPage, users, following, pages, loading } = useSelector((state) => state.userData);
+    
+    // Fetch users using RTK Query
+    const { data, isLoading, isFetching, error, refetch } = useFetchListOfUsersQuery(
+        { page: currentPage },
+        { refetchOnMountOrArgChange: true }
+    );
 
+    // Reset users state when component mounts
     useEffect(() => {
         dispatch(resetUsers());
     }, [dispatch]);
 
-    if (isLoading && currentPage === 1) return <div>Loading...</div>;
-    if (fetchError) return <div>Error loading users</div>;
+    // Load more users when "Show more users" is clicked
+    const loadMoreUsers = () => {
+        if (currentPage < pages) {
+            dispatch(setCurrentPage(currentPage + 1));
+        }
+    };
+
+    // Refresh users list
+    const refreshUsers = () => {
+        dispatch(resetUsers());
+        dispatch(setCurrentPage(1));
+        refetch();
+    };
+
+    if (isLoading && currentPage === 1) {
+        return <div className="content__loading">Loading users...</div>;
+    }
+
+    if (error) {
+        return (
+            <div className="content__error">
+                <h2>Error loading users</h2>
+                <p>{error.message || 'An unexpected error occurred'}</p>
+                <button className="content__button" onClick={refreshUsers}>
+                    Try again
+                </button>
+            </div>
+        );
+    }
 
     return (
         <>
             <header className="content__header">
                 <h1 className="content__title">People</h1>
+                <button 
+                    className="content__button" 
+                    onClick={refreshUsers}
+                    disabled={isFetching}
+                >
+                    {isFetching ? 'Refreshing...' : 'Refresh'}
+                </button>
             </header>
+
             <UserList
                 users={users}
-                currentPage={currentPage}
-                following={following}
-                loading={isLoading}
+                getUsers={loadMoreUsers}
+                loading={isFetching}
                 more={currentPage < pages}
             />
+            
             <br />
         </>
     )
